e2323db92804051dd09f8a68b59f88587682ff1f
Mr. Noob <103577823+Noobie-3@users.noreply.github.com>
2022-08-02 21:28:25 -0400

    Add files via upload

--- phpschoolstuff/registration.php
+++ phpschoolstuff/registration.php
@@ -1,27 +1,56 @@
 <?php
-    if(!isset($username) && !isset($password))
-    {
-        $username = " ";
-        $password = " ";
+
+    class Register {
+            public $FirstName = " ";
+            public $LastName = " ";
+            public $Email = " ";
+            public $password = " ";
+
+
+
+        
     }
+
+    $User1 = new Register();
+
 ?>
 
-<form action="validateRegister.php" method="post">
-    <label for="username"><b>Username</b></label><br>
-    <input type="text" placeholder="Enter Username" name="username"
-     value="<?php htmlspecialchars($username)?>"><br>
 
-    <?php if(isset($name_error)) {?>
-        <p><?php echo $name_error?></p>
+
+<form action="validateRegister.php" method="post">
+    <label for="FirstName"><b>First Name</b></label><br>
+    <input type="text" placeholder="Enter First name" name="FirstName"
+     value="<?php htmlspecialchars($User1->FirstName)?>"><br>
+
+    <?php if(isset($FirstName_error)) {?>
+        <p><?php echo $FirstName_error?></p>
     <?php }?>
+
+    <label for="LastName"><b>Last Name</b></label><br>
+    <input type="text" placeholder="Enter Last Name" name="LastName" 
+    value="<?php htmlspecialchars($User1->LastName)?>"><br>
+    
+    <?php if(isset($LastName_error)) {?>
+        <p><?php echo $LastName_error?></p>
+    <?php }?>
+
+    <label for="Email"><b>Email</b></label><br>
+    <input type="Email" placeholder="Enter Email" name="Email" 
+    value="<?php htmlspecialchars($User1->Email)?>"><br>
+
+    <?php if(isset($Email_error)) {?>
+        <p><?php echo $Email_error?></p>
+    <?php }?>
+
 
     <label for="password"><b>Password</b></label><br>
     <input type="password" placeholder="Enter Password" name="password" 
-    value="<?php htmlspecialchars($password)?>"><br>
+    value="<?php htmlspecialchars($User1->password)?>"><br>
     
     <?php if(isset($pass_error)) {?>
         <p><?php echo $pass_error?></p>
     <?php }?>
+
 
     <input type="submit" value="login"> 
    
--- phpschoolstuff/validateRegister.php
+++ phpschoolstuff/validateRegister.php
@@ -2,34 +2,47 @@
     //$username = $_POST['username'];
     //$password = $_POST['password'];
 
-    $username = filter_input(INPUT_POST, 'username');
+    $FirstName = filter_input(INPUT_POST, 'FirstName');
+    $LastName = filter_input(INPUT_POST, 'LastName');
+    $Email = filter_input(INPUT_POST, 'Email');
     $password = filter_input(INPUT_POST, 'password');
 
 
-    if(empty($username))
+    if(empty($FirstName)) 
     {
-        $name_error = "Please enter a username";
+        $FirstName_error = "Please enter a First Name";
     }
-    elseif(strlen($username) < 6)
-    {
-        $name_error = "Must have a minimum of 6 characters";
+    elseif(strlen($LastName) < 3) {
+        $FirstName_error = "Must have a minimum of 3 characters";
     }
 
-    if(empty($password))
-    {
-        $pass_error = "Please enter a password";
+    if(empty($LastName)) {
+        $LastName_error = "Please enter a Last Name";
     }
-    elseif(strlen($password) < 6)
-    {
+    elseif(strlen($LastName) < 3) {
+        $LastName_error = "Must have a minimum of 3 characters";
+    }
+
+    if(empty($Email)) {
+        $Email_error = "Please enter an Email address";
+    }
+    elseif (!filter_var($Email, FILTER_VALIDATE_EMAIL)) {
+        $Email_error = "Invalid email format";
+    }
+
+    
+    if(empty($password)) {
+        $pass_error = "Please enter a Password";
+    }
+    elseif(strlen($password) < 6) {
         $pass_error = "Must have a minimum of 6 characters";
     }
 
-    if(empty($name_error) && empty($pass_error))
-    {
-        include('success.php');
+    if(empty($FirstName_error) && (empty($LastName_error) && empty($pass_error))) {
+        include('successRegister.php');
     }
     else
-        include('index.php');
+        include('registration.php');
 
 
   
